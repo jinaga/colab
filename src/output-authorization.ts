6da862b33143fa9f35db581ad7bb814586d2d00b
Michael L Perry <michael@qedcode.com>
2023-08-02 17:23:06 -0500

    Post the authorization rules to the replicator

--- src/output-authorization.ts
+++ src/output-authorization.ts
@@ -10,4 +10,23 @@ const authorization = (a: AuthorizationRules) => a
   .with(taskAuthorization)
   ;
 
-console.log(describeAuthorizationRules(model, authorization));
+const postData = describeAuthorizationRules(model, authorization);
+const url = process.argv[2];
+const bearerToken = process.argv[3];
+
+// POST the authorization rules to the replicator.
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "text/plain",
+    "Authorization": `Bearer ${bearerToken}`
+  },
+  body: postData
+}).then(response => {
+  if (response.status === 201) {
+    console.log("Authorization rules updated.");
+  } else {
+    console.log(`Error updating authorization rules: ${response.status} ${response.statusText}`);
+    process.exit(1);
+  }
+});
